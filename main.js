5cf78415a45549fb71a72ef9d1983b3a31776cf8
walkerr <athangshende@gmail.com>
2026-05-29 20:37:48 +0530

    debug

--- main.js
+++ main.js
@@ -393,6 +393,7 @@ async function fetchRefs(angleData) {
   try {
     const res = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.pexels.com/v1/search?query=${query}&per_page=6&orientation=portrait&authorization=${PEXELS_KEY}`)}`);
     const data = await res.json();
+    console.log('pexels data:', data);
     grid.innerHTML = '';
     data.photos.forEach(photo => {
       const img = document.createElement('img');
